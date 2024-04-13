5ce7f035a036adfa6b54646ad2fcbf93fdbc8df5
Drew Zinck <dlzincktheii@gmail.com>
2024-04-12 22:37:41 -0700

    Add Roles

--- database/seed.sql
+++ database/seed.sql
@@ -8,3 +8,16 @@ VALUES
     ('Product Management'),
     ('Marketing'),
     ('Sales');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Software Engineer', 120000, 1),
+    ('Product Manager', 140000, 2),
+    ('Marketing Specialist', 110000, 3),
+    ('Sales Representative', 100000, 4),
+    ('Data Scientist', 130000, 1),
+    ('UX Designer', 115000, 1),
+    ('Technical Program Manager', 150000, 2),
+    ('Social Media Manager', 105000, 3),
+    ('Sales Manager', 160000, 4);
