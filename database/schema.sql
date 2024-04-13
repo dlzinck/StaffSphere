14ba5f1bbd4d2120a6e75227d4c163e3deb0afcc
Drew Zinck <dlzincktheii@gmail.com>
2024-04-12 21:23:08 -0700

    Implement Role Table Schema Definition

--- database/schema.sql
+++ database/schema.sql
@@ -12,3 +12,12 @@ CREATE TABLE department (
   id SERIAL PRIMARY KEY,
   name VARCHAR(30) UNIQUE NOT NULL
 );
+
+-- Create the role table
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
